ce800db073f1a07dc0e8e2d44934be01b0d3e4cd
David Nash <david.allyxanddar.nash@gmail.com>
2015-07-20 12:50:55 -0400

    working on gui

--- main.js
+++ main.js
@@ -1,58 +1,58 @@
 console.log("main.js responding");
 
 // array that stores the entire deck
-var fullDeck = [{face: "ace", val: 11, "suit": "clubs"},
-				 {face: "king", val: 10, "suit": "clubs"},
-				 {face: "queen", val: 10, "suit": "clubs"},
-				 {face: "jack", val: 10, "suit": "clubs"},
-				 {face: "ten", val: 10, "suit": "clubs"},
-				 {face: "nine", val: 9, "suit": "clubs"},
-				 {face: "eight", val: 8, "suit": "clubs"},
-				 {face: "seven", val: 7, "suit": "clubs"},
-				 {face: "six", val: 6, "suit": "clubs"},
-				 {face: "five", val: 5, "suit": "clubs"},
-				 {face: "four", val: 4, "suit": "clubs"},
-				 {face: "three", val: 3, "suit": "clubs"},
-				 {face: "two", val: 2, "suit": "clubs"},
-				 {face: "ace", val: 11, "suit": "hearts"},
-				 {face: "king", val: 10, "suit": "hearts"},
-				 {face: "queen", val: 10, "suit": "hearts"},
-				 {face: "jack", val: 10, "suit": "hearts"},
-				 {face: "ten", val: 10, "suit": "hearts"},
-				 {face: "nine", val: 9, "suit": "hearts"},
-				 {face: "eight", val: 8, "suit": "hearts"},
-				 {face: "seven", val: 7, "suit": "hearts"},
-				 {face: "six", val: 6, "suit": "hearts"},
-				 {face: "five", val: 5, "suit": "hearts"},
-				 {face: "four", val: 4, "suit": "hearts"},
-				 {face: "three", val: 3, "suit": "hearts"},
-				 {face: "two", val: 2, "suit": "hearts"},
-				 {face: "ace", val: 11, "suit": "spades"},
-				 {face: "king", val: 10, "suit": "spades"},
-				 {face: "queen", val: 10, "suit": "spades"},
-				 {face: "jack", val: 10, "suit": "spades"},
-				 {face: "ten", val: 10, "suit": "spades"},
-				 {face: "nine", val: 9, "suit": "spades"},
-				 {face: "eight", val: 8, "suit": "spades"},
-				 {face: "seven", val: 7, "suit": "spades"},
-				 {face: "six", val: 6, "suit": "spades"},
-				 {face: "five", val: 5, "suit": "spades"},
-				 {face: "four", val: 4, "suit": "spades"},
-				 {face: "three", val: 3, "suit": "spades"},
-				 {face: "two", val: 2, "suit": "spades"},
-				 {face: "ace", val: 11, "suit": "diamonds"},
-				 {face: "king", val: 10, "suit": "diamonds"},
-				 {face: "queen", val: 10, "suit": "diamonds"},
-				 {face: "jack", val: 10, "suit": "diamonds"},
-				 {face: "ten", val: 10, "suit": "diamonds"},
-				 {face: "nine", val: 9, "suit": "diamonds"},
-				 {face: "eight", val: 8, "suit": "diamonds"},
-				 {face: "seven", val: 7, "suit": "diamonds"},
-				 {face: "six", val: 6, "suit": "diamonds"},
-				 {face: "five", val: 5, "suit": "diamonds"},
-				 {face: "four", val: 4, "suit": "diamonds"},
-				 {face: "three", val: 3, "suit": "diamonds"},
-				 {face: "two", val: 2, "suit": "diamonds"}];
+var fullDeck = [{face: "ace", val: 11, "suit": "clubs", "url": "ace_of_clubs.png"},
+				 {face: "king", val: 10, "suit": "clubs", "url": "king_of_clubs.png"},
+				 {face: "queen", val: 10, "suit": "clubs", "url": "queen_of_clubs.png"},
+				 {face: "jack", val: 10, "suit": "clubs", "url": "jack_of_clubs.png"},
+				 {face: "ten", val: 10, "suit": "clubs", "url": "10_of_clubs.png"},
+				 {face: "nine", val: 9, "suit": "clubs", "url": "9_of_clubs.png"},
+				 {face: "eight", val: 8, "suit": "clubs", "url": "8_of_clubs.png"},
+				 {face: "seven", val: 7, "suit": "clubs", "url": "7_of_clubs.png"},
+				 {face: "six", val: 6, "suit": "clubs", "url": "6_of_clubs.png"},
+				 {face: "five", val: 5, "suit": "clubs", "url": "5_of_clubs.png"},
+				 {face: "four", val: 4, "suit": "clubs", "url": "4_of_clubs.png"},
+				 {face: "three", val: 3, "suit": "clubs", "url": "3_of_clubs.png"},
+				 {face: "two", val: 2, "suit": "clubs", "url": "2_of_clubs.png"},
+				 {face: "ace", val: 11, "suit": "hearts", "url": "ace_of_hearts.png"},
+				 {face: "king", val: 10, "suit": "hearts", "url": "king_of_hearts.png"},
+				 {face: "queen", val: 10, "suit": "hearts", "url": "queen_of_hearts.png"},
+				 {face: "jack", val: 10, "suit": "hearts", "url": "jack_of_hearts.png"},
+				 {face: "ten", val: 10, "suit": "hearts", "url": "10_of_hearts.png"},
+				 {face: "nine", val: 9, "suit": "hearts", "url": "9_of_hearts.png"},
+				 {face: "eight", val: 8, "suit": "hearts", "url": "8_of_hearts.png"},
+				 {face: "seven", val: 7, "suit": "hearts", "url": "7_of_hearts.png"},
+				 {face: "six", val: 6, "suit": "hearts", "url": "6_of_hearts.png"},
+				 {face: "five", val: 5, "suit": "hearts", "url": "5_of_hearts.png"},
+				 {face: "four", val: 4, "suit": "hearts", "url": "4_of_hearts.png"},
+				 {face: "three", val: 3, "suit": "hearts", "url": "3_of_hearts.png"},
+				 {face: "two", val: 2, "suit": "hearts", "url": "2_of_hearts.png"},
+				 {face: "ace", val: 11, "suit": "spades", "url": "ace_of_spades.png"},
+				 {face: "king", val: 10, "suit": "spades", "url": "king_of_spades.png"},
+				 {face: "queen", val: 10, "suit": "spades", "url": "queen_of_spades.png"},
+				 {face: "jack", val: 10, "suit": "spades", "url": "jack_of_spades.png"},
+				 {face: "ten", val: 10, "suit": "spades", "url": "10_of_spades.png"},
+				 {face: "nine", val: 9, "suit": "spades", "url": "9_of_spades.png"},
+				 {face: "eight", val: 8, "suit": "spades", "url": "8_of_spades.png"},
+				 {face: "seven", val: 7, "suit": "spades", "url": "7_of_spades.png"},
+				 {face: "six", val: 6, "suit": "spades", "url": "6_of_spades.png"},
+				 {face: "five", val: 5, "suit": "spades", "url": "5_of_spades.png"},
+				 {face: "four", val: 4, "suit": "spades", "url": "4_of_spades.png"},
+				 {face: "three", val: 3, "suit": "spades", "url": "3_of_spades.png"},
+				 {face: "two", val: 2, "suit": "spades", "url": "2_of_spades.png"},
+				 {face: "ace", val: 11, "suit": "diamonds", "url": "ace_of_diamonds.png"},
+				 {face: "king", val: 10, "suit": "diamonds", "url": "king_of_diamonds.png"},
+				 {face: "queen", val: 10, "suit": "diamonds", "url": "queen_of_diamonds.png"},
+				 {face: "jack", val: 10, "suit": "diamonds", "url": "jack_of_diamonds.png"},
+				 {face: "ten", val: 10, "suit": "diamonds", "url": "10_of_diamonds.png"},
+				 {face: "nine", val: 9, "suit": "diamonds", "url": "9_of_diamonds.png"},
+				 {face: "eight", val: 8, "suit": "diamonds", "url": "8_of_diamonds.png"},
+				 {face: "seven", val: 7, "suit": "diamonds", "url": "7_of_diamonds.png"},
+				 {face: "six", val: 6, "suit": "diamonds", "url": "6_of_diamonds.png"},
+				 {face: "five", val: 5, "suit": "diamonds", "url": "5_of_diamonds.png"},
+				 {face: "four", val: 4, "suit": "diamonds", "url": "4_of_diamonds.png"},
+				 {face: "three", val: 3, "suit": "diamonds", "url": "3_of_diamonds.png"},
+				 {face: "two", val: 2, "suit": "diamonds", "url": "2_of_diamonds.png"}];
 
 // gives click events to hit and stand buttons
 $("#hit").click(function() {
@@ -90,6 +90,12 @@ function dealCard(recipient) {
 	fullDeck.pop(rand);
 	console.log(rand);
 	handEval(recipient);
+	if (recipient === playerHand) {
+		$("#ph").append("<img id='cardimg' src='./assets/cards/" + rand.url + "' />");
+	}
+	if (recipient === dealerHand) {
+		$("#dh").append("<img id='cardimg' src='./assets/cards/" + rand.url + "' />");
+	}
 	// this function will deal another card to the specified recipient
 	// take a random sample from the deck array and push it into the hand array, while popping it from the deck
 	// it should also check the new value of the hand in question
@@ -212,4 +218,9 @@ function dealerTurn() {
 	}
 	// this function will activate a dealer's turn when the player has chosen to stand
 	// the dealer must continue to draw cards until he either beats the player's hand or busts
+}
+
+function cardTest() {
+	$("#ph").append("<img id='cardimg' src='./assets/cards/2_of_diamonds.png' />");
+	// $("<div>").attr("class", "card").css("background-image", "url(./assets/cards/2_of_clubs.png)").appendTo("#ph");
 }
